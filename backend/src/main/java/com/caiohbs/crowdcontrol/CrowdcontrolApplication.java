d271be0c13a5f9d6779c5f774b40b11aa70a3a20
Caio Bianchi <caiohbs23@gmail.com>
2024-04-19 11:56:01 -0300

    first backend commit

---
+++ backend/src/main/java/com/caiohbs/crowdcontrol/CrowdcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.caiohbs.crowdcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdcontrolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/caiohbs/crowdcontrol/CrowdcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiohbs.crowdcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
